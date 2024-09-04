8443a34f1d91711bc83bae1cfda58c1f097c5532
Kodomo At <guillaume@staub.pro>
2024-09-04 10:18:56 +0200

    🚧 Routes : Add basics routes with basics pages

---
+++ app/community/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CommunityPage = () => {
+    return (
+        <h1>
+            Community page
+
+        </h1>
+    );
+};
+
+export default CommunityPage;
---
+++ app/share/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SharePage = () => {
+    return (
+        <h1>
+            Share Page
+        </h1>
+    );
+};
+
+export default SharePage;
