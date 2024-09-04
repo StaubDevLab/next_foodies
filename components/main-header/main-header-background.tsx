0c5f4a45044f86451aa44b4875eab1ca478c5f31
Kodomo At <guillaume@staub.pro>
2024-09-04 17:28:38 +0200

    🔨 Remove props unused in components: main-header, main-header-background, nav-link

--- components/main-header/main-header-background.tsx
+++ components/main-header/main-header-background.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './main-header-background.module.css';
-function MainHeaderBackground(props) {
+function MainHeaderBackground() {
     return (
         <div className={classes['header-background']}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
